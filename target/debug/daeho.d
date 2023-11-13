c7b32b8253339b640356cd74d7ebc6313d988047
Daeho Jang <dhjang4522@gmail.com>
2023-11-13 19:55:23 +0900

    JISANG SEXY GUY

---
+++ target/debug/daeho.d
@@ -0,0 +1,1 @@
+C:\Users\82106\Documents\rust\daeho\target\debug\daeho.exe: C:\Users\82106\Documents\rust\daeho\src\main.rs
---
+++ target/debug/deps/daeho.d
@@ -0,0 +1,5 @@
+C:\Users\82106\Documents\rust\daeho\target\debug\deps\daeho.exe: src\main.rs
+
+C:\Users\82106\Documents\rust\daeho\target\debug\deps\daeho.d: src\main.rs
+
+src\main.rs:
